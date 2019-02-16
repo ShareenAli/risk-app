623175f919dec9f0944bbef572b55eddf87ab354
Shareen Ali <ali.shareen19@gmail.com>
2019-02-15 20:10:57 -0500

    Initialize the Settings UI

--- src/game/settings/SettingsController.java
+++ src/game/settings/SettingsController.java
@@ -1,4 +1,34 @@
 package game.settings;
 
+import javax.swing.JFrame;
+
 public class SettingsController {
+    private SettingsView settingsView;
+    private JFrame settingsFrame = new JFrame();
+
+    public SettingsController() {
+        this.settingsView = new SettingsView();
+
+        this.initListeners();
+    }
+
+    public void initializeUi() {
+        this.settingsFrame.setContentPane(this.settingsView.$$$getRootComponent$$$());
+        this.settingsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.bindListeners();
+    }
+
+    public void displayUi() {
+        this.settingsFrame.pack();
+        this.settingsFrame.setVisible(true);
+    }
+
+    private void bindListeners() {
+
+    }
+
+    private void initListeners() {
+
+    }
 }
