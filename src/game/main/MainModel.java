e357fac325c8c4806e364c7959d341ba1d4a966f
Shareen Ali <ali.shareen19@gmail.com>
2019-03-01 20:12:33 -0500

    Initialized the main view

--- src/game/main/MainModel.java
+++ src/game/main/MainModel.java
@@ -1,4 +1,30 @@
 package game.main;
 
-public class MainModel {
+import entity.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Observable;
+
+@SuppressWarnings("deprecation")
+public class MainModel extends Observable {
+    private HashMap<String, Player> players = new HashMap<>();
+
+    public MainModel() { }
+
+    public void setPlayers(ArrayList<Player> playerList) {
+        for (Player player : playerList) {
+            this.players.put(player.getName(), player);
+        }
+    }
+
+    public Player getPlayer(String name) {
+        return this.players.get(name);
+    }
+
+    public void updatePlayer(String name, Player player) {
+        this.players.put(name, player);
+        setChanged();
+        notifyObservers(player);
+    }
 }
