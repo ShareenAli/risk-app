9031269505409c2897d1f58ad283e0410fd0955f
farhan <farhan.wasee@northsouth.edu>
2019-03-02 13:23:47 -0500

    Modification of Continent

--- src/entity/Continent.java
+++ src/entity/Continent.java
@@ -1,15 +1,18 @@
 package entity;
 
+import java.util.ArrayList;
+
 /**
  * This entity class holds all the information about the continent.
  * It also includes the methods to manipulate the information of the continent
- * @author shareenali
+ * @author shareenali, farhan
  * @version 0.1
  */
 
 public class Continent {
     private String name;
     private int controlValue;
+	ArrayList<Territory> territoriesIn;
 
     /**
      * It holds the continent data.
@@ -38,4 +41,32 @@ public class Continent {
     public int getControlValue() {
         return controlValue;
     }
+	
+	/**
+     * It returns the latest control value of the continent
+     * When a player rules over the entire continent,
+     *  he is given the no of armies equal to this value in the reinforcement phase.
+     * @return controlValue
+     */
+    public int getControlValue() {
+        return controlValue;
+    }
+	
+	/**
+     * It returns the territories held in this continent
+     * A continent must hold at least one territory.
+     * @return listOfTerritories
+     */
+    public ArrayList<Territory> getTerritoriesIn() {
+		return territoriesIn;
+	}
+	
+	/**
+     * It sets the list of territories currently belonging to the continent
+     * A continent must hold at least one territory.
+	 * @param territoriesIn list of territories in the continent
+     */
+	public void setTerritoriesIn(ArrayList<Territory> territoriesIn) {
+		this.territoriesIn = territoriesIn;
+	}
 }
